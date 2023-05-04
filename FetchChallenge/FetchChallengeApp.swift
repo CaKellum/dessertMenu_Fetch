d9b4059f203b041e3a937ef980f1c68dc30c31b1
CaKellum <christopherk379@gmail.com>
2023-05-03 21:08:48 -0400

    Initial Commit

---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Christoper Kellum on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
