87295b1cf4fee79c9c9ae9a29b014d3790a6ec38
CaKellum <christopherk379@gmail.com>
2023-05-04 22:43:31 -0400

    Add gitignore

---
+++ FetchChallenge/Views/ItemView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemView.swift
+//  FetchChallenge
+//
+//  Created by Christoper Kellum on 5/4/23.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemView()
+    }
+}
---
+++ FetchChallenge/Views/ViewModels/MenuViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MenuViewViewModel.swift
+//  FetchChallenge
+//
+//  Created by Christoper Kellum on 5/4/23.
+//
+
+import Foundation
